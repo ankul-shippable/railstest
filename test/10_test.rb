779a8fe6d4a9631df79d189145f3e01b40feac24
ankul-shippable <ankul@shippable.com>
2019-03-07 16:20:29 +0530

    Update 10_test.rb

--- test/10_test.rb
+++ test/10_test.rb
@@ -3,7 +3,12 @@ require "test_helper"
 class A10Test < Minitest::Test
 
   def test_it_waits_for_10_seconds
-    sleep 1
+    sleep 60
+    assert true
+  end
+  
+  def test_it_waits_for_10_seconds
+    sleep 60
     assert true
   end
 end
