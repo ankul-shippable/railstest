81e2716a08a669f7cebac808d28640199caf746e
ankul-shippable <ankul@shippable.com>
2019-03-07 16:20:00 +0530

    Update 09_test.rb

--- test/09_test.rb
+++ test/09_test.rb
@@ -3,7 +3,12 @@ require "test_helper"
 class A09Test < Minitest::Test
 
   def test_it_waits_for_9_seconds
-    sleep 1
+    sleep 60
+    assert true
+  end
+  
+  def test_it_waits_for_9_seconds
+    sleep 60
     assert true
   end
 end
